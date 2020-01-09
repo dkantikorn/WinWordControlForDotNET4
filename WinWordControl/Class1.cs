67e11f72e803f29328236d4409ad47bcc53191f0
DK_Antikorn <sarawutt.b@pakgon.com>
2020-01-09 11:47:55 +0700

    Add project files.

---
+++ WinWordControl/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinWordControl
+{
+    public class Class1
+    {
+    }
+}
